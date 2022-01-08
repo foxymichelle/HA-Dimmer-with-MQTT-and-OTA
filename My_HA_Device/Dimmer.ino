a014c351193e20bd79ec8987f5ea25fd8525fcc9
foxymichelle <57567635+foxymichelle@users.noreply.github.com>
2022-01-07 18:13:39 -0800

    Create Dimmer.ino

---
+++ My_HA_Device/Dimmer.ino
@@ -0,0 +1,153 @@
+#include "hw_timer.h"
+
+/****************************** DIMMER SETUP *******************************************/
+// Do not edit these variables
+
+int state = 0;                  // 0 = off; 1 = on
+byte tarBrightness = 0;         // target brightness used in ISR
+byte curBrightness = 0;         // value controled by pwm as light transtions to target
+byte zcState = 0;               // 0 = ready; 1 = processing; used in ISR
+int Percent = 0;                // Percent = % Brightness
+int inTransition = 0;           // 0 = No transition in progress; 1 = Rising; 2 = Falling
+int incrementT = 0;             // transition time increments
+int minB = 1;                   // min Brigtness
+int maxB = 99;                  // max Brightness
+int SkipStart = 1;              // Used in ISR to avoid flicker
+unsigned long time_now = 0;     // Millis() function
+
+void ICACHE_RAM_ATTR zcDetectISR ();
+void ICACHE_RAM_ATTR dimTimerISR ();
+
+
+/********************************* DIMMER SETUP & Loop ***************************************/
+
+void dimmerSetup() {
+  pinMode(zcPin, INPUT_PULLUP);
+  pinMode(pwmPin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(zcPin), zcDetectISR, RISING);
+  hw_timer_init(NMI_SOURCE, 0);
+  hw_timer_set_func(dimTimerISR);
+  minB = minPercent*254/100;  // Recalculate min/max brightness value for
+  maxB = maxPercent*254/100;  // time increment per step conversions
+}
+
+void dimmerLoop() {
+  if (inTransition != 0) {
+    if(millis() > time_now + incrementT){
+      time_now = millis();
+      if (curBrightness == NewBrightness) {
+        inTransition = 0;
+        Serial.println("Transition complete!");
+      }
+      else if (inTransition == 1) {
+        if (curBrightness == maxB) {
+          inTransition = 0;
+          tarBrightness = NewBrightness;
+        }
+        else {
+          ++tarBrightness;
+        }
+      }
+      else if (inTransition == 2) {
+        if (curBrightness == minB) {
+          inTransition = 0;
+          tarBrightness = NewBrightness;
+        }
+        else {
+          --tarBrightness;
+        }
+      }
+      SkipStart = 1;
+      dimTimerISR();
+    }
+  }
+}
+
+/************************************* DIMMER TRIGGER *****************************************/
+
+void dimmerTrigger() {
+  inTransition = 0; // Cancels active transtions
+  Percent = NewBrightness*100/255;  // Calculate % brightness
+  Serial.println("Set Light Data:");
+  Serial.print("Percent: ");Serial.print(Percent);Serial.print("%");
+  if (Percent >= 1) {
+    OldBrightness = NewBrightness;
+    if (Percent <= 99) {
+      NewBrightness = ((maxB-minB)*Percent/100)+minB;  // Recalculate target brightness based
+    }                                                  // on the user defined min and max
+  }
+  Serial.print(", New Target: ");Serial.print(NewBrightness);
+  if (NewBrightness == 100) {
+    incrementT = transitionTime*1000/(curBrightness-maxB);
+  }
+  else if (NewBrightness == 0) {
+    incrementT = transitionTime*1000/(curBrightness-minB);
+    inTransition = 2;
+    tarBrightness = --curBrightness;
+  }
+  else {
+    incrementT = transitionTime*1000/(curBrightness-NewBrightness);
+    if (incrementT < 1) {
+      incrementT = abs(incrementT);
+      inTransition = 1;
+      if (curBrightness == 0) {
+        tarBrightness = minB;
+      }
+      else {
+        tarBrightness = ++curBrightness;
+      }
+    }
+    else {
+      inTransition = 2;
+      if (curBrightness == 255) {
+        tarBrightness = maxB;
+      }
+      else {
+        tarBrightness = --curBrightness;
+      }
+    }
+  }
+  Serial.print(", Transition incrementT: ");Serial.println(incrementT);
+  time_now = millis();
+  SkipStart = 1;
+  dimTimerISR();
+}
+
+/********************************* DIMMER ISRs ******************************************/
+
+void dimTimerISR() {
+  if (curBrightness > tarBrightness) {
+    --curBrightness;
+  }
+  else if (curBrightness < tarBrightness) {
+    ++curBrightness;
+    if (state = 0) {
+      state = 1;
+    }
+  }
+  if (curBrightness <= 2) {
+    state = 0;
+    digitalWrite(pwmPin, 0);
+  }
+  else {
+    if (SkipStart == 0) {
+      digitalWrite(pwmPin, 1);
+      zcState = 0;
+    }
+    else {                //****** Runs a non-changing first cycle to avoid flicker
+      SkipStart = 0;
+      zcState = 0;
+    }
+  }
+}
+
+void zcDetectISR() {
+  if (zcState == 0) {
+    zcState = 1;
+    if (curBrightness < 250) {
+      digitalWrite(pwmPin, 0);
+      int dimDelay = 30 * (255 - curBrightness) + 400;//400
+      hw_timer_arm(dimDelay);
+    }
+  }
+}
