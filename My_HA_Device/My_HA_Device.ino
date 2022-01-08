9656867c862e068d803d4d7947780fb651f839eb
foxymichelle <57567635+foxymichelle@users.noreply.github.com>
2022-01-07 18:11:47 -0800

    Update My_HA_Device.ino

--- My_HA_Device/My_HA_Device.ino
+++ My_HA_Device/My_HA_Device.ino
@@ -1,1 +1,84 @@
-test
+/********************************* HA & MQTT CONNECTION ***************************************/
+
+#define serial_baud     115200
+const char* ssid = "YourWifi";
+const char* password = "WifiPassord";
+const char* mqtt_server = "YourHAIP";       // IP of the MQTT broker/home assisstant server
+const char* mqtt_username = "username";   // HA user created for Mosquitto Broker add-on
+const char* mqtt_password = "password";   // HA user password
+
+/********************************** MULTIPLE DEVICE SETUP  ***********************************
+Maintian one file, then simply change the DeviceID before updating to each madule.
+
+For LIGHT_1, etc, match the friendly name you will use in your HA config.yaml*/
+
+int DeviceID = 4;  // IMPORTANT: Which device will be loaded on this module? (1 = LIGHT_1, etc.)
+
+// Edit the items below
+
+const byte zcPin = 12;              // your zero-cross-over pin
+const byte pwmPin = 13;             // your pwm pin
+const char* LIGHT_1 = "LR Device";  // Living Room device name
+const char* LIGHT_2 = "Kitchen";    // Kitchen device name
+const char* LIGHT_3 = "Master";     // Master room device name
+const char* LIGHT_4 = "Patio";      // Patio device name
+// Add or remove lights as needed
+
+// DO NOT EDIT the items below (edit these in the setup function below instead!)
+
+const char* mqttName = "xx";
+const char* dimmerTopic = "xx";
+const char* dimmerCommand = "xx";
+int minPercent = 0;
+int maxPercent = 0;
+
+/********************************* SETUP & LOOP **********************************************
+The collection of files may contain only ONE setup and loop function. Those are
+located here, triggering setup and loop functions in each file. 
+
+For each dimmer device, define the specific values below:
+  - ignore mqtt name - it sets it to the name identified above
+  - MQTT dimmer topic and command
+  - min & maxPercent - test how your lights respond at the default 1-99%, note optimal
+    low value and optimal high value and enter those below. When you set the light to 1-99%,
+    the actual bightness produced will fall in your desired range. 
+    NOTE that 0 is always off; 100 is alwasy full on, despite the range you specify) */
+
+void setup() {
+  if (DeviceID == 1) {
+    mqttName = LIGHT_1;                      // Do NOT edit (uses variable name already created)
+    dimmerTopic = "livingroom/dimmer";       // MQTT topic string
+    dimmerCommand = "livingroom/dimmer/set"; // ALWAYS use topic string + "/set"
+    minPercent = 8;                          // Specify device min...
+    maxPercent = 58;                         // ...and max after testing
+  }
+  else if (DeviceID == 2) {
+    mqttName = LIGHT_2;
+    dimmerTopic = "kitchen/dimmer";
+    dimmerCommand = "kitchen/dimmer/set";
+    minPercent = 8;
+    maxPercent = 58;
+  }
+  else if (DeviceID == 3) {
+    mqttName = LIGHT_3;
+    dimmerTopic = "master/dimmer";
+    dimmerCommand = "master/dimmer/set";
+    minPercent = 1;
+    maxPercent = 99;
+  }
+  else if (DeviceID == 4) {
+    mqttName = LIGHT_4;
+    dimmerTopic = "patio/dimmer";
+    dimmerCommand = "patio/dimmer/set";
+    minPercent = 1;
+    maxPercent = 99;
+  }
+  // Add or remove "else if..." segements for each device as needed
+  wifiSetup();
+  dimmerSetup();
+}
+
+void loop() {
+  wifiLoop();
+  dimmerLoop();
+}
