a112b625fe046caabdf7872cfbb8697c1385c71a
foxymichelle <57567635+foxymichelle@users.noreply.github.com>
2023-06-23 06:18:27 -0700

    Update Dimmer.ino
    See 6/2023 updates

--- My_HA_Device/Dimmer.ino
+++ My_HA_Device/Dimmer.ino
@@ -7,20 +7,23 @@ OR a rapid movement from 100 to max brightness when falling/0 to min brightness 
 It is NOT coded to handle the full range: 0-100/100-0. I always sunrise to a mid-range 
 percent, or have the light set to a gentle dim % when it starts the sunset. */
 
-// Do not edit these variables
+/****** Do NOT edit these variables ******/
 
-int state = 0;                  // 0 = off; 1 = on
-byte tarBrightness = 0;         // target brightness used in ISR
-byte curBrightness = 0;         // value controled by pwm as light transtions to target
-byte zcState = 0;               // 0 = ready; 1 = processing; used in ISR
-int Percent = 0;                // Percent = % Brightness
-int inTransition = 0;           // 0 = No transition in progress; 1 = Rising; 2 = Falling
-int incrementT = 0;             // transition time increments
-int minB = 1;                   // min Brigtness
-int maxB = 99;                  // max Brightness
-int SkipStart = 1;              // Used in ISR to avoid flicker
-unsigned long time_now = 0;     // Millis() function
-
+// byte state = 0;                  // 0 = off; 1 = on - MOVED to communication page
+byte tarBrightness = 0;          // target brightness used in ISR
+byte curBrightness = 0;          // value controled by pwm as light transtions to target
+byte zcState = 0;                // 0 = ready; 1 = processing; used in ISR
+byte Percent = 0;                // Percent = % Brightness
+byte inTransition = 0;           // 0 = No transition in progress; 1 = Rising; 2 = Falling
+int incrementT = 0;              // transition time increments
+byte minB = 1;                   // min Brigtness
+byte maxB = 99;                  // max Brightness
+byte SkipStart = 1;              // used in ISR to avoid flicker
+unsigned long transtionTime = 0; // millis() function
+bool dimISRworking = false;      // the next 3 variables check if ISRs are running when the
+bool zcISRworking = false;       // dimmer is on; if there is an interupt conflict that stops
+unsigned long ISRtime = 0;       // the process -which won't restart again on its own- then
+                                 // the ISRs are reset to resume proper dimmer function.
 void ICACHE_RAM_ATTR zcDetectISR ();
 void ICACHE_RAM_ATTR dimTimerISR ();
 
@@ -28,22 +31,47 @@ void ICACHE_RAM_ATTR dimTimerISR ();
 /********************************* DIMMER SETUP & Loop ***************************************/
 
 void dimmerSetup() {
+  void ICACHE_RAM_ATTR zcDetectISR ();  // included here as well for dimmer reset (after conflict error shutdown)
+  void ICACHE_RAM_ATTR dimTimerISR ();
   pinMode(zcPin, INPUT_PULLUP);
   pinMode(pwmPin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(zcPin), zcDetectISR, RISING);
   hw_timer_init(NMI_SOURCE, 0);
   hw_timer_set_func(dimTimerISR);
+  DEBUGLN("dimmer ISRs turned on");
   minB = minPercent*254/100;  // Recalculate min/max brightness value for
   maxB = maxPercent*254/100;  // time increment per step conversions
 }
 
+void dimmerShutdown() {       // for dmmer reset
+  state = 0;
+  timer1_disable();
+  detachInterrupt(digitalPinToInterrupt(zcPin));
+  digitalWrite(pwmPin, 0);
+}
+
 void dimmerLoop() {
-  if (inTransition != 0) {
-    if(millis() > time_now + incrementT){
-      time_now = millis();
+  if (state == 1) {                             // Only runs if the dimmer is on
+    if (currentMillis - ISRtime >= 1500){
+      if (zcISRworking && dimISRworking) {      // Every 1.5s, check working bools and reset to
+        ISRtime = millis();                     // false; functioning ISRs will make true again.
+        zcISRworking = false;
+        dimISRworking = false;
+        DEBUG("* ");
+      }
+      else {                                    // if ISRs failed and did not make true,
+        DEBUGLN("dimmer FAILED");               // then reset dimmer ISRs.
+        dimmerShutdown();
+        delay(100);
+        dimmerSetup();
+        dimmerTrigger();
+      }
+    }
+    if (inTransition != 0 && currentMillis - transtionTime >= incrementT) {
+      transtionTime = millis();
       if (curBrightness >= NewBrightness-1 && curBrightness <= NewBrightness+1) {
         inTransition = 0;
-        Serial.println("Transition complete!");
+        DEBUGLN("Transition complete!");
       }
       else if (inTransition == 1) {
         if (curBrightness >= maxB) { // Rapid finish from maxB to 100%, which will also end cycle
@@ -63,80 +91,106 @@ void dimmerLoop() {
           --tarBrightness;
         }
       }
-      SkipStart = 1;
+      SkipStart = 1;                  // Stops the flicker!
       dimTimerISR();
     }
+    checkMQTT();
   }
 }
 
 /************************************* DIMMER TRIGGER *****************************************/
 
 void dimmerTrigger() {
-  inTransition = 0; // Cancels active transtions
+  DEBUGLN("Start dimmerTrigger");
+  inTransition = 0;                 // Cancels active transitions
   Percent = NewBrightness*100/255;  // Calculate % brightness
-  Serial.println("Set Light Data:");
-  Serial.print("Percent: ");Serial.print(Percent);Serial.print("%");
+  DEBUGLN("Set Light Data:");
+  DEBUG("Percent: ");DEBUG(Percent);DEBUG("%");
   if (Percent >= 1) {
+    if (state == 0) {
+      dimmerSetup();                // Get ISRs running again
+    }
     OldBrightness = NewBrightness;  // Save brightness for toggle off/on to override HA on at 0
-  }
-  Serial.print(", New Target: ");Serial.print(NewBrightness);
-  if (Percent == 100) {             // Calculate transtion time to 100% using maxB
-    incrementT = transitionTime*1000/(curBrightness-maxB);
-  }
-  else if (Percent == 0) {          // Calculate time to 0% using minB
-    incrementT = transitionTime*1000/(curBrightness-minB);
-  }
-  else {            // Calculate 1-99% new brightness based on min/max, then transition time
-    NewBrightness = ((maxB-minB)*Percent/100)+minB;
-    incrementT = transitionTime*1000/(curBrightness-NewBrightness);
-  }
-  if (incrementT < 0) {             // Megative transition time = brightness rising
-    incrementT = abs(incrementT);   // Make transition time positive
-    inTransition = 1;
-    if (curBrightness == 0) {       // Jump to minB from 0, then use transition time...
-      tarBrightness = minB;
+    if (Percent == 100) {           // Set 100 to slightly less than 255 to try to fix over-dimming
+      NewBrightness = 245;
     }
-    else {
-      ++tarBrightness;              // ...or start rising from current percent
+    else {                          // Calculate 1-99% new brightness based on min/max
+      NewBrightness = ((maxB-minB)*Percent/100)+minB;
     }
+  }
+  DEBUG(", New Target: ");DEBUG(NewBrightness);
+  if (transitionTime <= 1) {
+    tarBrightness = NewBrightness;
+    incrementT = 0; 
   }
   else {
-    inTransition = 2;
-    if (curBrightness == 255) {
-      tarBrightness = maxB;
+    if (Percent == 100) {            // Calculate transtion time to 100% using maxB
+      incrementT = transitionTime*1000/(curBrightness-maxB);
+    }
+    else if (Percent == 0) {         // Calculate time to 0% using minB
+      incrementT = transitionTime*1000/(curBrightness-minB);
     }
     else {
-      --tarBrightness;
+      incrementT = transitionTime*1000/(curBrightness-NewBrightness);
+    }
+    if (incrementT < 0) {            // Negative transition time = brightness rising
+      incrementT = abs(incrementT);  // Make transition time positive
+      inTransition = 1;
+      if (curBrightness == 0) {      // Jump to minB from 0, then use transition time...
+        tarBrightness = minB;
+      }
+      else {
+        ++tarBrightness;             // ...or start rising from current percent
+      }
+    }
+    else {
+      inTransition = 2;
+      if (curBrightness == 255) {
+        tarBrightness = maxB;
+      }
+      else {
+        --tarBrightness;
+      }
     }
   }
-  Serial.print(", Transition incrementT: ");Serial.println(incrementT);
-  time_now = millis();               // Start first transition time timer
+  DEBUG(", Transition incrementT: ");DEBUGLN(incrementT);
+  currentMillis = millis();
+  transtionTime = millis();          // Start first transition timer
+  ISRtime = millis();
   SkipStart = 1;                     // to avoid flicker in dimTimerISR
   dimTimerISR();                     // Start process
+  DEBUGLN("End dimmerTrigger");
 }
 
 /********************************* DIMMER ISRs ******************************************/
 
 void dimTimerISR() {
-  if (curBrightness > tarBrightness) {
-    --curBrightness;
-  }
-  else if (curBrightness < tarBrightness) {
-    ++curBrightness;
-    if (state = 0) {
-      state = 1;
+  dimTimer();
+}
+
+void dimTimer() {
+  dimISRworking = true;              // In loop, checks if working
+  if (curBrightness != tarBrightness) {
+    if (curBrightness > tarBrightness) {
+      --curBrightness;
+    }
+    else if (curBrightness < tarBrightness) {
+      ++curBrightness;
+      if (state == 0) {
+        state = 1;
+      }
     }
   }
-  if (curBrightness <= 2) {
-    state = 0;
-    digitalWrite(pwmPin, 0);
+  if (deviceState[1] == 'F' && curBrightness <= 2) {
+    dimmerShutdown();
+    DEBUGLN("Dimmer turned off - run dimmerShutdown");
   }
   else {
     if (SkipStart == 0) {
       digitalWrite(pwmPin, 1);
       zcState = 0;
     }
-    else {                //****** Runs a non-changing first cycle to avoid flicker
+    else {                           // Runs a non-changing first cycle to avoid flicker
       SkipStart = 0;
       zcState = 0;
     }
@@ -144,11 +198,16 @@ void dimTimerISR() {
 }
 
 void zcDetectISR() {
+  zcDetect();
+}
+
+void zcDetect() {
+  zcISRworking = true;
   if (zcState == 0) {
     zcState = 1;
     if (curBrightness < 250) {
       digitalWrite(pwmPin, 0);
-      int dimDelay = 30 * (255 - curBrightness) + 400;//400
+      int dimDelay = 30 * (255 - curBrightness) + 400;
       hw_timer_arm(dimDelay);
     }
   }
