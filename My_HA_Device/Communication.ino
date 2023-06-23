569d16c46ea32dcc06fef8c761acfde77f34c60b
foxymichelle <57567635+foxymichelle@users.noreply.github.com>
2023-06-23 06:28:25 -0700

    Update Communication.ino
    See 6/2023 Updates

--- My_HA_Device/Communication.ino
+++ My_HA_Device/Communication.ino
@@ -8,36 +8,56 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Do not edit these variables
-bool reboot = true;
-const char* deviceState = "OFF";// Used for MQTT / Command
-int transitionTime = 1;         // Defaults to 1 when transition time not received
-int NewBrightness = 0;          // Brightness = value between 1-256
-int OldBrightness = 0;          // Resets brightness to last state when light toggled off/on
+#define DEBUGSTATE 0 // 1 on; 0 off - turn off when not using serial monitor to debug
+#if DEBUGSTATE == 1
+#define DEBUG(x) Serial.print(x)
+#define DEBUGLN(x) Serial.println(x)
+#else
+#define DEBUG(x)
+#define DEBUGLN(x)
+#endif
+
+/****** Do NOT edit these variables ******/
+
+const char* deviceState = "OFF"; // Used for MQTT / Command
+byte transitionTime = 1;         // Defaults to 1 when transition time not received
+byte NewBrightness = 0;          // Brightness = value between 1-256
+byte OldBrightness = 0;          // Resets brightness to last state when light toggled off/on
+bool firstBoot = true;           // Used to reset dimmer timers/interrupts on reconnect & cancel long transition
+const char* wifiState = "OFF";   // Used for MQTT / Command - notify wifi reconnect / force restart
+unsigned long reconnectStart = 0;         // Millis used for both wifi timeout and reconnect intervals
+const int wifiInterval = 16000;           // Gives 8 seconds to connect to wifi
+const int connectInterval = 60000;        // Wait 60 seconds before trying again
+bool sendBus = false;
+byte state = 0;                  // dimmer on or off - only checks for OTA when off!
+
 
 /************************* SETUP - triggered in main setup function **************************/
 
 void wifiSetup() {
   Serial.begin(serial_baud);
-  wifiConnect();
+  delay(100);
+  DEBUGLN("Starting setup...");
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  wifiConnect();
   
   // OTA Stuff
   if (DeviceID == 1) {
-    ArduinoOTA.setHostname("Living Room Device");
+    ArduinoOTA.setHostname(DEVICE_1);
   }
   else if (DeviceID == 2) {
-    ArduinoOTA.setHostname("Kitchen Device");
+    ArduinoOTA.setHostname(DEVICE_2);
   }
   else if (DeviceID == 3) {
-    ArduinoOTA.setHostname("Foxy Device");
+    ArduinoOTA.setHostname(DEVICE_3);
   }
   else if (DeviceID == 4) {
-    ArduinoOTA.setHostname("Patio Device");
+    ArduinoOTA.setHostname(DEVICE_4);
   }
   ArduinoOTA.onStart([]() {
-    timer1_disable();                   /************** Stops timer interupts for OTA ***********/
+    timer1_disable();                              /****** Stops timer interupts for OTA ******/
+    detachInterrupt(digitalPinToInterrupt(zcPin));
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
@@ -71,40 +91,90 @@ void wifiSetup() {
 
 /**************************** LOOP - triggered in main setup function ************************/
 void wifiLoop() {
-  if (WiFi.status() != WL_CONNECTED) wifiConnect();
-  if (!client.connected()) {
-    reconnect();
+  if (WiFi.status() != WL_CONNECTED && currentMillis - reconnectStart >= connectInterval) {
+    WiFi.disconnect();
+    // dimmerShutdown();       // created for testing; not needed but saved for reference
+    aht10Reset();
+    delay(500);
+    wifiConnect();
   }
-  client.loop();
-  ArduinoOTA.handle();
+  if (WiFi.status() == WL_CONNECTED) {
+    if (!client.connected()) {
+      if (currentMillis - reconnectStart >= connectInterval) {
+        reconnect();
+      }
+    }
+    else {
+      if (sendBus) {
+        sendBus = false;
+        sendBusData();
+      }
+    }
+    if (state == 0) {           // *** ONLY checks for OTA updates when dimmer is OFF!! Update will fail if dimmer is on ***
+      ArduinoOTA.handle();
+    }
+  }
+  checkMQTT();
+}
+
+void checkMQTT() {
+  if (WiFi.status() == WL_CONNECTED) {
+    client.loop();
+  }
 }
 
 /************************************ WIFI CONNECT *****************************************/
-void wifiConnect(){
-  Serial.println("Connecting to wifi");
+
+void wifiConnect() {
+  DEBUGLN("Connecting to wifi");
+  currentMillis = millis();
+  reconnectStart = millis();   // Set "now" for connection timeout
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    DEBUG(".");
+    if (currentMillis - reconnectStart >= wifiInterval) {  // if timeouts, stop process and wait to try again
+      WiFi.disconnect();
+      DEBUGLN("Wifi failed to connect");
+      //ESP.restart();
+      break;
+    }
   }
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP()); 
+  reconnectStart = millis();   // Reset "now" for loop
+  if (WiFi.status() == WL_CONNECTED) {
+    DEBUGLN("WiFi connected");
+    DEBUG("IP address: ");
+    DEBUGLN(WiFi.localIP());
+    reconnect();               // Connect MQTT to avoid the bypass wait interval in loop
+    wifiState = "OFF";         // will turn back ON and try to update and verify the new
+    sendwifiNotice();          // connection state with the device.
+    if (!firstBoot) {
+      dimmerSetup();           // dimmer interupts only need reset if reconnecting to internet.
+      dimmerTrigger();
+      aht10Reset();
+    }
+  }
 }
 
 /************************************** MQTT CONNECT *****************************************/
 
-void reconnect(){
-  if (client.connect(mqttName, mqtt_username, mqtt_password)) {
-    Serial.println("Connected to MQTT Broker");
+void reconnect() {
+  if (client.connect(mqttName, mqtt_username, mqtt_password, availablityTopic, 2, true, "offline")) {
+    DEBUGLN("Connected to MQTT Broker");
+    client.publish(availablityTopic, "online", true);
     client.subscribe(dimmerTopic);
     client.subscribe(dimmerCommand);
+    client.subscribe(deviceTopic);
+    client.subscribe(deviceCommand);
+    if (DeviceID == 1 || DeviceID == 4) {
+      client.subscribe(aht10Topic);
+    }
   }
   else {
-    Serial.println("Connection to MQTT Broker failed...");
-    // Set up a 5000ms countdown to try again in 5 seconds?
+    DEBUGLN("Connection to MQTT Broker failed...");
   }
+  reconnectStart = millis();
 }
 
 /*********************************** MQTT CALLBACK *******************************************/
@@ -112,35 +182,51 @@ void reconnect(){
 void callback(char* topic, byte* payload, unsigned int length) {
   String Topic = String(topic);              // Check if the word "set" is in the
   int checkTopic = Topic.indexOf("set");     // topic; filters out the device's
-  if (checkTopic != -1) {                    // published/confirmation messages (sendState).
-    Serial.print("Message arrived from ");
-    Serial.println(topic);
- 
+  if (checkTopic != -1) {                    // sent messages.
+    DEBUG("Message arrived from ");
+    DEBUGLN(topic);
+
     StaticJsonDocument <256> doc;
     DeserializationError error = deserializeJson(doc, payload);
     if (error) {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.c_str());
+      DEBUG(F("deserializeJson() failed: "));
+      DEBUGLN(error.c_str());
       return;
     }
+    
+    int checkTopic = Topic.indexOf("dimmer");     // Check if dimmer
+    if (checkTopic != -1) {
+      deviceState = doc["state"];
+      transitionTime = doc["transition"];
+      NewBrightness = doc["brightness"];
 
-    deviceState = doc["state"];
-    transitionTime = doc["transition"];
-    NewBrightness = doc["brightness"];
-
-    if (transitionTime <= 1 || reboot) {
-      reboot = false;
-      transitionTime = 1;
+      if (transitionTime <= 1 || firstBoot) {
+        firstBoot = false;
+        transitionTime = 1;
+      }
+      if (deviceState[1] == 'N' && NewBrightness == 0) {
+        NewBrightness = OldBrightness;
+      }
+      DEBUG("Command: {state: "); DEBUG(deviceState);
+      DEBUG(", transition: "); DEBUG(transitionTime);
+      DEBUG(", brightness: "); DEBUG(NewBrightness); 
+      DEBUGLN("}");
+      sendState();     // First send a message back to confirm message received,
+      dimmerTrigger(); // then trigger command, which will adjust for min/max brightness
     }
-    if (deviceState[1] == 'N' && NewBrightness == 0) {
-      NewBrightness = OldBrightness;
+    else {                    // Not dimmer; confirming device connection or triggering reboot
+      wifiState = doc["state"];
+      DEBUG("Command: {state: "); DEBUG(wifiState);
+      DEBUGLN("}");
+      if (wifiState[1] == 'N') {
+        sendwifiNotice();
+      }
+      else {
+        sendwifiNotice();   // First send a message back to confirm message received,
+        delay(1000);
+        ESP.reset();
+      }
     }
-    Serial.print("Command: {state: "); Serial.print(deviceState);
-    Serial.print(", transition: "); Serial.print(transitionTime);
-    Serial.print(", brightness: "); Serial.print(NewBrightness); 
-    Serial.println("}");
-    sendState();     // First send a message back to confirm message received,
-    dimmerTrigger(); // then trigger command, which will adjust for min/max brightness
   }
 }
 
@@ -153,7 +239,17 @@ void sendState() {
   doc["transition"] = transitionTime;
   doc["brightness"] = NewBrightness;
   size_t n = serializeJson(doc, buffer);
-  Serial.print("Publish: ");
-  Serial.println(buffer);
+  DEBUG("Publish: ");
+  DEBUGLN(buffer);
   client.publish(dimmerTopic, buffer, n);
+}
+
+void sendwifiNotice() {
+  char buffer[256];
+  StaticJsonDocument<256> doc;
+  doc["state"] = wifiState;
+  size_t n = serializeJson(doc, buffer);
+  DEBUG("Publish: ");
+  DEBUGLN(buffer);
+  client.publish(deviceTopic, buffer, n);
 }
